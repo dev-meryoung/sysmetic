625686f17dd1b407a7458c7fa905927ea2f5f63b
hjkwon <sirru97@naver.com>
2024-11-05 13:55:07 +0900

    fix: 파일 내보내기 방식 수정 #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
 import router from '@/routes/Router';
 
-export const App = () => (
+const App = () => (
   <>
     <RouterProvider router={router} />
   </>
 );
+
+export default App;
