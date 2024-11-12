16279451cc14b2173e86b731b350b887425ffcf0
meryoung <dev.meryoung@gmail.com>
2024-11-12 12:14:42 +0900

    settings: 상수 파일(color, font) 세팅 #42

---
+++ src/constants/font.ts
@@ -0,0 +1,25 @@
+export const FONT_SIZE = {
+  TITLE_XXL: '4.8rem',
+  TITLE_XL: '4.0rem',
+  TITLE_LG: '3.2rem',
+  TITLE_MD: '2.8rem',
+  TITLE_SM: '2.4rem',
+  TITLE_XS: '2.0rem',
+  TEXT_LG: '1.8rem',
+  TEXT_MD: '1.6rem',
+  TEXT_SM: '1.4rem',
+  TEXT_XS: '1.2rem',
+  TEXT_XXS: '1.0rem',
+};
+
+export const FONT_WEIGHT = {
+  THIN: 100,
+  EXTRA_LIGHT: 200,
+  LIGHT: 300,
+  REGULAR: 400,
+  MEDIUM: 500,
+  SEMI_BOLD: 600,
+  BOLD: 700,
+  EXTRA_BOLD: 800,
+  BLACK: 900,
+};
