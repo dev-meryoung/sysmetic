623f86d6ae53989358c71127ab650f975980d02a
hjkwon <sirru97@naver.com>
2024-11-15 18:11:50 +0900

    feat: totopbutton 컴포넌트 구현

---
+++ src/components/ToTopButton.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { css } from '@emotion/react';
+import NorthIcon from '@mui/icons-material/North';
+import IconButton from '@/components/IconButton';
+
+interface ToTopButtonProps {
+  handleClick?: () => void;
+}
+
+const ToTopButton: React.FC<ToTopButtonProps> = ({ handleClick }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (!handleClick) {
+      setIsVisible(window.scrollY > 300);
+    }
+  };
+
+  const handleToTop = () => {
+    if (handleClick) {
+      handleClick();
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (!handleClick) {
+      window.addEventListener('scroll', toggleVisibility);
+      toggleVisibility();
+    } else {
+      setIsVisible(true);
+    }
+
+    return () => {
+      if (!handleClick) {
+        window.removeEventListener('scroll', toggleVisibility);
+      }
+    };
+  }, [handleClick, toggleVisibility]);
+
+  if (!isVisible) return null;
+
+  return (
+    <div css={toTopButtonStyle}>
+      <IconButton
+        IconComponent={NorthIcon}
+        color='primary'
+        iconBgSize='lg'
+        handleClick={handleToTop}
+      />
+    </div>
+  );
+};
+
+const toTopButtonStyle = css`
+  position: fixed;
+  bottom: 80px;
+  right: calc(50% - 566px);
+  transform: translateX(-50%);
+`;
+
+export default ToTopButton;
