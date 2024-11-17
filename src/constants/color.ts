c111d146b615166481281d64bec43152dd3e6cea
LeeYunHwan <cyzhqly@gmail.com>
2024-11-17 16:08:40 +0900

    chore: color.ts 최신화 #24

--- src/constants/color.ts
+++ src/constants/color.ts
@@ -32,14 +32,6 @@ export const COLOR = {
   WARN_YELLOW: '#f9a825',
   ERROR_RED: '#b41c11',
   INFO_BLUE: '#0082d3',
-  GRAY_100: '#f9f9f9',
-  GRAY_200: '#eee',
-  GRAY_300: '#eaeaea',
-  GRAY_400: '#e5e5e5',
-  GRAY_500: '#ccc',
-  GRAY_600: '#aaa',
-  GRAY_700: '#787878',
-  GRAY_800: '#333',
 };
 
 export const COLOR_OPACITY = {
