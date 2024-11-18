c1f4136e2a049ffb396c7ff9348e7253d28566cd
LeeYunHwan <cyzhqly@gmail.com>
2024-11-18 12:03:31 +0900

    feat: onChange 추가하여 부모에서 선택 값 추적 가능하도록 설정 #25

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -12,12 +12,14 @@ interface SelectBoxProps {
   width?: number;
   placeholder?: string;
   options: SelectOptionProps[];
+  onChange: (value: string) => void;
 }
 
 const SelectBox: React.FC<SelectBoxProps> = ({
   width = 175,
   options,
   placeholder = '선택하세요.',
+  onChange,
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isSelected, setIsSelected] = useState('');
@@ -29,6 +31,7 @@ const SelectBox: React.FC<SelectBoxProps> = ({
   const handleSelectOption = (option: string) => {
     setIsSelected(option);
     setIsOpen(false);
+    onChange?.(option);
   };
 
   return (
