911dfe1b33f7462d6c94e72e04bde8a16be70f15
LeeYunHwan <cyzhqly@gmail.com>
2024-11-22 22:35:55 +0900

    refactor: 회원가입 페이지 공통 컴포넌트 및 상수로 교체 #109

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,4 +1,7 @@
 import { css } from '@emotion/react';
+import Button from '@/components/Button';
+import { COLOR, COLOR_OPACITY } from '@/constants/color';
+import { FONT_SIZE, FONT_WEIGHT } from '@/constants/font';
 
 const SignUp = () => (
   <div css={wrapperStyle}>
@@ -11,11 +14,27 @@ const SignUp = () => (
         시스메틱 홈페이지의 회원이 되시면 전략탐색, 전략등록 등 다양한 서비스를
         이용하실 수 있습니다.
       </p>
-      <p>투자자 가입으로 가입 시, 관심있는 전략만 별도로 관리할 수 있습니다.</p>
+      <p>투자자로 가입 시, 관심있는 전략만 별도로 관리할 수 있습니다.</p>
+      <p>트레이더로 가입 시, 자신의 전략을 저장하고 공유할 수 있습니다.</p>
     </div>
     <div css={buttonDivStyle}>
-      <button>투자자 가입</button>
-      <button>트레이더 가입</button>
+      <Button
+        size='xl'
+        fontWeight={700}
+        fontSize='32px'
+        width={580}
+        label='투자자 가입'
+        handleClick={() => console.log('투자자')}
+      />
+      <Button
+        size='xl'
+        fontWeight={700}
+        fontSize='32px'
+        width={580}
+        color='primary700'
+        label='트레이더 가입'
+        handleClick={() => console.log('트레이더')}
+      />
     </div>
     <div css={alertDivStyle}>
       <p>* 일반회원은 투자자로 가입하시면 됩니다.</p>
@@ -24,36 +43,36 @@ const SignUp = () => (
 );
 
 const wrapperStyle = css`
+  position: relative;
   width: 100%;
   height: 100%;
   max-width: 1200px;
   padding: 0 10px;
-  margin: 0 auto;
-  color: #000;
+  margin: 96px auto 96px;
   letter-spacing: -0.4px;
 `;
 
 const titleStyle = css`
   width: 82px;
   height: 24px;
-  font-weight: 700;
-  font-size: 24px;
+  font-weight: ${FONT_WEIGHT.BOLD};
+  font-size: ${FONT_SIZE.TITLE_SM};
   line-height: 24px;
-  margin: 96px 0 40px;
+  margin-bottom: 40px;
 `;
 
 const contentsDivStyle = css`
-  border: 1px solid #1261c44d;
+  border: 1px solid ${COLOR_OPACITY.PRIMARY_OPACITY10};
   padding: 30px 26px;
 
   h2 {
-    font-size: 20px;
+    font-size: ${FONT_SIZE.TITLE_XS};
     margin-bottom: 11px;
   }
 
   h2 > span {
-    color: #1261c4;
-    font-weight: 700;
+    color: ${COLOR.PRIMARY};
+    font-weight: ${FONT_WEIGHT.BOLD};
   }
 
   p {
@@ -62,37 +81,21 @@ const contentsDivStyle = css`
 `;
 
 const buttonDivStyle = css`
-  margin-top: 40px;
   display: flex;
-  justify-content: space-between;
-  gap: 22px;
+  gap: 20px;
+  margin-top: 40px;
 
-  button {
-    width: 578px;
-    height: 136px;
-    font-family: inherit;
-    font-size: 32px;
-    color: #fff;
-    font-weight: 700;
-    line-height: 160%;
-    line-spacing: -0.64px;
-    background-color: #1261c4;
-    // background-color: #0B3C79;
-    cursor: pointer;
-    border: none;
-    border-radius: 4px;
+  p {
+    color: ${COLOR.POINT};
   }
 `;
 
 const alertDivStyle = css`
-  width: 258px;
-  height: 26px;
-  color: #c84a31;
-  font-size: 16px;
-  font-weight: 400;
+  color: ${COLOR.POINT};
+  font-size: ${FONT_SIZE.TEXT_MD};
   line-height: 160%;
   letter-spacing: -0.32px;
-  margin: 7px 0 105px 321px;
+  padding-top: 8px;
 `;
 
 export default SignUp;
