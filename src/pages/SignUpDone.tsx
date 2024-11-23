8a9c5c564ab10023bd8e9939ed484b6ccdbf6332
LeeYunHwan <cyzhqly@gmail.com>
2024-11-23 16:49:16 +0900

    chore: 회원가입 완료 페이지 안내문구 수정 #109

--- src/pages/SignUpDone.tsx
+++ src/pages/SignUpDone.tsx
@@ -1,10 +1,12 @@
 import { css } from '@emotion/react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import profileImg from '@/assets/images/default-profile.png';
 import Button from '@/components/Button';
 import { FONT_SIZE, FONT_WEIGHT } from '@/constants/font';
 import { PATH } from '@/constants/path';
+
 const SignUpDone = () => {
+  const { type } = useParams();
   const navigate = useNavigate();
 
   const handleMainBtnClick = () => {
@@ -22,11 +24,19 @@ const SignUpDone = () => {
       </div>
       <div css={contentsDivStyle}>
         <h2>축하합니다. Sysmetic 회원가입이 완료되었습니다!</h2>
-        <p>
-          Sysmetic과 함께 투자하는 전략들을 만나보세요. <br />
-          좋은 전략을 Follow 하고, 문의하기를 통해 직접 트레이더에게 문의하실 수
-          있습니다.
-        </p>
+        {type === 'investor' ? (
+          <p>
+            Sysmetic과 함께 다양한 투자 전략들을 만나보세요. <br />
+            좋은 전략을 Follow 하고, 문의하기를 통해 직접 트레이더에게 문의하실
+            수 있습니다.
+          </p>
+        ) : (
+          <p>
+            Sysmetic을 통해 다양한 투자 전략들을 관리해 보세요. <br />
+            회원님의 전략을 공유하고, 문의 및 댓글을 통해 직접 투자자들과
+            소통하실 수 있습니다.
+          </p>
+        )}
       </div>
       <div css={buttonDivStyle}>
         <Button
